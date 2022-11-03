fb9d998c91e3fe68aa12ff7f053cad256856a948
Arnas.Kazlauskas <arnas.kazlauskas@kilo.health>
2022-11-03 16:15:33 +0200

    feat(pangram): solution

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,1 +1,22 @@
 package pangram
+
+import (
+	"strings"
+)
+
+func IsPangram(input string) bool {
+	input = strings.ToLower(input)
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	inputMap := make(map[rune]bool)
+
+	for _, v := range input {
+		inputMap[v] = true
+	}
+
+	for _, v := range alphabet {
+		if _, found := inputMap[v]; !found {
+			return false
+		}
+	}
+	return true
+}
