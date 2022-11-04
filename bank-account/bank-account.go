ba22042537983c4adb13cb3f333604db98ee76ad
Arnas.Kazlauskas <arnas.kazlauskas@kilo.health>
2022-11-04 14:49:14 +0200

    feat(bank-account): solution

--- bank-account/bank-account.go
+++ bank-account/bank-account.go
@@ -1,13 +1,14 @@
 package account
 
-import "math"
+import (
+	"sync"
+)
 
 type Account struct {
 	active  bool
 	balance int64
+	mu      sync.Mutex
 }
-
-// Define the Account type here.
 
 func Open(amount int64) *Account {
 	if amount < 0 {
@@ -17,38 +18,38 @@ func Open(amount int64) *Account {
 }
 
 func (a *Account) Balance() (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
 	if !a.active {
 		return 0, false
 	}
 	return a.balance, true
 }
 
+func (a *Account) deposit(amount int64) {
+	a.balance += amount
+}
+
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	if !a.active {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if !a.active || a.balance+amount < 0 {
 		return 0, false
 	}
-	if amount >= 0 {
-		a.balance += amount
-		return a.balance, true
-	}
-
-	d := int64(math.Abs(float64(amount)))
-
-	if a.balance < d {
-		return 0, false
-	}
-
-	a.balance -= d
+	a.balance += amount
 	return a.balance, true
 }
 
 func (a *Account) Close() (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
 	if !a.active {
 		return 0, false
 	}
-	var b int64
 	a.active = false
-	b, a.balance = a.balance, 0
 
-	return b, true
+	return a.balance, true
 }
--- main.go
+++ main.go
@@ -3,10 +3,57 @@ package main
 import (
 	account "exercism/bank-account"
 	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
-	a := account.Open(100)
-	a.Close()
+	a := account.Open(0)
+	const amt = 10
+	const c = 1000
+
+	var negBal int32
+	var start, g sync.WaitGroup
+
+	start.Add(1)
+	g.Add(3 * c)
+
+	for i := 0; i < c; i++ {
+		go func() { // deposit
+			start.Wait()
+			a.Deposit(amt) // ignore return values
+			g.Done()
+		}()
+		go func() { // withdraw
+			start.Wait()
+			for {
+				if _, ok := a.Deposit(-amt); ok {
+					break
+				}
+				time.Sleep(time.Microsecond) // retry
+			}
+			g.Done()
+		}()
+		go func() { // watch that balance stays >= 0
+			start.Wait()
+			if p, _ := a.Balance(); p < 0 {
+				atomic.StoreInt32(&negBal, 1)
+			}
+			g.Done()
+		}()
+	}
+	start.Done()
+	g.Wait()
+
+	if negBal == 1 {
+		fmt.Println("Balance went negative with concurrent deposits and " +
+			"withdrawals.  Want balance always >= 0.")
+	}
+	if p, ok := a.Balance(); !ok || p != 0 {
+		fmt.Printf("After equal concurrent deposits and withdrawals, "+
+			"a.Balance = %d, %t.  Want 0, true", p, ok)
+	}
+
 	fmt.Println(a.Balance())
 }
